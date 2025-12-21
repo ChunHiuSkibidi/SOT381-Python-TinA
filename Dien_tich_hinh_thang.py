4f52dc7a5e30efeeb5ab5776ecaf235a303fb55d
ChunHiuSkibidi <hatrunghieu291207@gmail.com>
2025-12-21 21:58:38 +0700

    tính diện tích hình thang
    Viết chương trình tính diện tích hình thang (với đáy lớn, đáy bé, chiều cao nhập vào).

---
+++ Dien_tich_hinh_thang.py
@@ -0,0 +1,5 @@
+daylon = float (input("Nhập đáy lớn: "))
+daybe = float (input("Nhập đáy bé: "))
+h = float (input("Nhập chiều cao: "))
+s = (daylon + daybe) * h / 2
+print (f"DIện tích hình thang là: {s}")
