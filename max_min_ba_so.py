d2f9e96be52127b5cc0d818f7e708cbc62f26500
ChunHiuSkibidi <hatrunghieu291207@gmail.com>
2025-12-22 14:51:31 +0700

    tìm min max 3 số

--- max_min_ba_so.py
+++ max_min_ba_so.py
@@ -1,3 +1,15 @@
 a = float(input("Nhập số thứ nhất:"))
 b = float(input("Nhập số thứ hai:"))
 c = float(input("Nhập số thứ ba:"))
+max = a
+if b > max:
+    max = b
+if c > max:
+    max = c
+print(f"Số lớn nhất là: {max}")
+min = a
+if b < min:
+    min = b
+if c < max:
+    min = c
+print(f"Số nhỏ nhất là: {min}")    
