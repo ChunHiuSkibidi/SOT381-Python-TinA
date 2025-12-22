37911b9393fe9d0a5c6a5f59065d5133ada26d8e
ChunHiuSkibidi <hatrunghieu291207@gmail.com>
2025-12-22 14:24:46 +0700

    tính chu vi và diện tích có điều kiện

---
+++ Tinh_chu_vi_dien_tich_co_dk.py
@@ -0,0 +1,6 @@
+dai = float(input("Nhập chiều dài: "))
+rong = float(input("Nhập chiều rộng:"))
+chuvi = (dai + rong) * 2
+dt = dai * rong
+print(f"Chu vi HCN là:{chuvi:9.2f}")
+print(f"Diện tich HCN là:{dt:9.2f}")
