957b2bf289eb1375fee1c39dad1b470f551e5d40
ChunHiuSkibidi <hatrunghieu291207@gmail.com>
2025-12-22 15:51:14 +0700

    tìm min max 3 số

--- TH2212_Bai4.py
+++ TH2212_Bai4.py
@@ -1,21 +1,20 @@
-def so_lon_nhat(a, b, c):
+def so_lonnho_nhat(a, b, c):
     LN=a
     if b > LN:
         LN=b
     if c > LN:
         LN=c
-    return LN
-def so_nho_nhat(a, b, c):
+        
     NN=a
     if b < NN:
         NN=b
     if c < NN:
         NN=c
-    return NN
+    return LN, NN
 a = float(input("Nhập số thứ nhất:"))
 b = float(input("Nhập số thứ hai:"))
 c = float(input("Nhập số thứ ba:"))
-ket_qua1 = so_lon_nhat(a, b, c)
-ket_qua2 = so_nho_nhat(a, b, c)
+ket_qua1, kq2 = so_lonnho_nhat(a, b, c)
+
 print(f"Số lớn nhất trong ba số {a}, {b}, {c} là: {ket_qua1}")
-print(f"Số nhỏ nhất trong ba số {a}, {b}, {c} là: {ket_qua2}")
+print(f"Số nhỏ nhất trong ba số {a}, {b}, {c} là: {kq2}")
