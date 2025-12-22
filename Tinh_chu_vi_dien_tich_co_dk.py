25c5d78c4f75ab6663e317bdf503a0c518818066
ChunHiuSkibidi <hatrunghieu291207@gmail.com>
2025-12-22 14:39:19 +0700

    tính chu vi và diện tích HCN có ĐK
    dùng lệnh while

--- Tinh_chu_vi_dien_tich_co_dk.py
+++ Tinh_chu_vi_dien_tich_co_dk.py
@@ -1,6 +1,17 @@
-dai = float(input("Nhập chiều dài: "))
-rong = float(input("Nhập chiều rộng:"))
-chuvi = (dai + rong) * 2
-dt = dai * rong
-print(f"Chu vi HCN là:{chuvi:9.2f}")
-print(f"Diện tich HCN là:{dt:9.2f}")
+while True:
+  w = float(input("Nhập chiều dài: "))
+  if 0 <= w <= 100:
+      break
+  else:
+      print("Sai, nhập lại")
+while True:
+  h = float(input("Nhập chiều rộng:"))
+  if 0 <= h <= 100:
+      break
+  else:
+      print("Sai, nhập lại")      
+
+chuvi = (w + h) * 2
+dt = w * h
+print(f"Chu vi HCN là:{chuvi:.2f}")
+print(f"Diện tich HCN là:{dt:.2f}")
