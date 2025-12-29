e71d139a19f33a427c36f8f952ae48b2096b7509
ChunHiuSkibidi <hatrunghieu291207@gmail.com>
2025-12-29 14:50:38 +0700

    kết quả thi 3 môn

--- 3_diem_mon_thi.py
+++ 3_diem_mon_thi.py
@@ -6,6 +6,8 @@ if tong >= 15 and toan >= 4 and ly >= 4 and hoa >= 4:
     print("Đậu")
     if toan >= 5 and ly >= 5 and hoa >= 5:
         print("Học đều các môn")
-        else:
-            print("Học chua đều các môn")
+    else:
+            print("Học chưa đều các môn")
+else:
+        print("Thi hỏng")
             
